de55b5458bbd4585c57210ce7d5524c732757d76
Gulka_D <d.gulka4@gmail.com>
2021-09-19 16:11:03 +0200

    Connect local MongoDB

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express();
 const path = require('path');
 const cons = require('consolidate');
 const ejs = require('ejs');
+const mongoose = require('mongoose');
+
 
 const viewPath = path.join(__dirname, 'views');
 const port = 3000;
@@ -13,6 +15,15 @@ app.use(express.static(path.join(__dirname, 'static')))
 app.engine('html', cons.swig);
 app.set('views', viewPath);
 app.set('view engine', 'ejs');
+
+mongoose.connect('mongodb://localhost:27017/flashcards')
+  .then(() => {
+      console.log('DB connected')
+  })
+  .catch(err => {
+        console.log(err)
+  });
+
 
 app.get('/', (req, res) => {
     res.render('home')
